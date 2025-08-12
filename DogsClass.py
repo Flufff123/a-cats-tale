ffdf9939459be234bcb831d5552783e67e51203a
Emily <19lakinE@highcliffeschool.com>
2025-08-12 15:21:30 +0100

    Initial commit with only APK, HTML, and favicon

--- DogsClass.py
+++
@@ -1,7 +0,0 @@
-# Inherits from StaticEnemiesClass, EnemiesClass, and AssetsClass. No movement during game.
-
-from StaticEnemiesClass import staticEnemies
-
-class dogs(staticEnemies):
-    def __init__(self, visibleOnScreen, col, row, imageFileA, soundFile, name, tileWidth, index,basePath, imageFileB):
-        super().__init__(visibleOnScreen, col, row, imageFileA, soundFile, name, tileWidth, index,basePath, imageFileB)
--- LilliesClass.py
+++
@@ -1,7 +0,0 @@
-# Inherits from StaticEnemiesClass, EnemiesClass, and AssetsClass. No movement during game.
-
-from StaticEnemiesClass import staticEnemies
-
-class lillies(staticEnemies):
-    def __init__(self, visibleOnScreen, col, row, imageFileA, soundFile, name, tileWidth, index,basePath, imageFileB):
-        super().__init__(visibleOnScreen, col, row, imageFileA, soundFile, name, tileWidth, index,basePath, imageFileB)
